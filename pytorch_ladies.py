4e487e78cf9573ed79da33e8d7798359d8f42c22
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2023-09-12 13:40:24 +0200

    Change default config: 256 batch size, 256 sample size, 2 gcn layers

--- pytorch_ladies.py
+++ pytorch_ladies.py
@@ -28,15 +28,15 @@ parser.add_argument('--pool_num', type=int, default= 10,
                     help='Number of Pool')
 parser.add_argument('--batch_num', type=int, default= 10,
                     help='Maximum Batch Number')
-parser.add_argument('--batch_size', type=int, default=512,
+parser.add_argument('--batch_size', type=int, default=256,
                     help='size of output node in a batch')
-parser.add_argument('--n_layers', type=int, default=5,
+parser.add_argument('--n_layers', type=int, default=2,
                     help='Number of GCN layers')
 parser.add_argument('--n_iters', type=int, default=1,
                     help='Number of iteration to run on a batch')
 parser.add_argument('--n_stops', type=int, default=200,
                     help='Stop after number of batches that f1 dont increase')
-parser.add_argument('--samp_num', type=int, default=64,
+parser.add_argument('--samp_num', type=int, default=256,
                     help='Number of sampled nodes per layer')
 parser.add_argument('--sample_method', type=str, default='ladies',
                     help='Sampled Algorithms: ladies/fastgcn/full')
