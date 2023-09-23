beb813a5dae99501a5799888debb78cb17e66746
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2023-09-23 07:17:34 +0200

    Repeat 5 times for each sample size

--- run_experiments_ladies.sh
+++ run_experiments_ladies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=48:00:00
+#SBATCH --time=96:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=ladies
 #SBATCH --cpus-per-task=10
@@ -20,13 +20,13 @@ method="ladies"
 output_file1="results_table1.csv"
 output_file2="results_table2.csv"
 
-# Create the CSV files if they don't exist
-if [ ! -f $output_file1 ]; then
-    echo "Method,Dataset,Accuracy" > $output_file1
-fi
+## Create the CSV files if they don't exist
+#if [ ! -f $output_file1 ]; then
+#    echo "Method,Dataset,Accuracy(mean),Accuracy(std)" > $output_file1
+#fi
 
 if [ ! -f $output_file2 ]; then
-    echo "Method,Dataset,Sampling Number,Accuracy" > $output_file2
+    echo "Method,Dataset,Sampling Number,Accuracy(mean),Accuracy(std)" > $output_file2
 fi
 
 # Define the list of datasets
@@ -39,17 +39,20 @@ samp_nums=(32 64 128 256 512)
 for dataset in "${datasets[@]}"; do
     echo -e "\n\nProcessing ${dataset}\n\n"
 
-    # Experiment 1
-    python pytorch_ladies.py --cuda 0 --dataset "${dataset,,}" --runs 10 --sample_method ${method} --data_dir /var/scratch/tsingam/ > ${dataset}_${method}_exp1.output
+#    # Experiment 1 - Repeat 10 times
+#    python pytorch_ladies.py --cuda 0 --dataset "${dataset,,}" --runs 10 --sample_method ${method} --data_dir /var/scratch/tsingam/ > ${dataset}_${method}_exp1.output
+#
+#    mean=$(grep -Eo "Mini Acc: [0-9.]+ ± [0-9.]+" ${dataset}_${method}_exp1.output | awk '{print $3}')
+#    std=$(grep -Eo "Mini Acc: [0-9.]+ ± [0-9.]+" ${dataset}_${method}_exp1.output | awk '{print $5}')
+#    echo "$method,$dataset,$mean,$std" >> $output_file1
 
-    accuracy=$(grep -Eo "Mini Acc: [0-9.]+ ± [0-9.]+" ${dataset}_${method}_exp1.output)
-    echo "$method,$dataset,$accuracy" >> $output_file1
-
-    # Experiment 2
+    # Experiment 2 - Repeat 5 times
     for samp_num in "${samp_nums[@]}"; do
-        python3 pytorch_ladies.py --cuda 0 --dataset "${dataset,,}" --sample_method ${method} --samp_num "$samp_num" --data_dir /var/scratch/tsingam/ > ${dataset}_${method}_exp2.output
+        python3 pytorch_ladies.py --cuda 0 --dataset "${dataset,,}" --runs 5 --sample_method ${method} --samp_num "$samp_num" --data_dir /var/scratch/tsingam/ > ${dataset}_${method}_exp2.output
 
-        accuracy=$(grep -Eo "Mini Acc: [0-9.]+ " ${dataset}_${method}_exp2.output | awk '{print $3}')
-        echo "$method,$dataset,$samp_num,$accuracy" >> $output_file2
+        mean=$(grep -Eo "Mini Acc: [0-9.]+ ± [0-9.]+" ${dataset}_${method}_exp2.output | awk '{print $3}')
+        std=$(grep -Eo "Mini Acc: [0-9.]+ ± [0-9.]+" ${dataset}_${method}_exp2.output | awk '{print $5}')
+        echo "$method,$dataset,,$samp_num,$mean,$std" >> $output_file2
     done
 done
+
